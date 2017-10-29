f0e9bbf0c28dc1c7890de2370c04050d4630a0d6
Cameron Sharp <sharpcameron@hotmail.co.uk>
2017-10-29 12:50:53 +0000

    Feature: Lookup the owner of an AS Number

---
+++ ircbot/plugins/asnlookup.py
@@ -0,0 +1,23 @@
+import requests
+
+from ircbot import bot
+
+
+def get_asn_owner(asn):
+    endpoint = "https://api.bgpview.io/asn/{}".format(asn)
+
+    resp = requests.get(endpoint)
+
+    return resp.json()['data']['description_short']
+
+
+@bot.command('asn')
+def command(bot, channel, sender, args):
+    """Lookup the owner of an AS number. Usage: {bot.trigger}asn 1234"""
+    if len(args) < 1:
+        bot.message(channel, "This command requires an argument, e.g. {}asn 1234".format(bot.trigger))
+        return
+
+    owner = get_asn_owner(args[0])
+
+    bot.message(channel, "The owner of AS{} is {}".format(args[0], owner))
