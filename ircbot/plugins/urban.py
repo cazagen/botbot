c1594138427cb35201489d6c4bc5517bdf1ffbca
Cameron Sharp <sharpcameron@hotmail.co.uk>
2017-11-27 16:52:26 +0000

    substitute two spaces with one

--- ircbot/plugins/urban.py
+++ ircbot/plugins/urban.py
@@ -1,4 +1,5 @@
 import urbandictionary
+import re
 
 from ircbot import bot
 
@@ -17,7 +18,13 @@ def ud(bot, channel, sender, args):
 def udrandom(bot, channel, sender, args):
     """ Defines you a random word from urban dictonary"""
     definition = urbandictionary.random()
+    strip_empty(definition[0].definition)
     bot.message(channel, "{}: {}".format(
         definition[0].word,
         definition[0].definition
     ))
+
+def strip_empty(definition):
+    print(definition)
+    new_definition = re.sub(r'\s{2}', ' ', definition)
+    print(new_definition)
