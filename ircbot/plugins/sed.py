dfc356609f29569ef74c8126f38b8c7db821d0fc
Cameron Sharp <sharpcameron@hotmail.co.uk>
2018-05-09 14:44:04 +0100

    Use a global last message variable

--- ircbot/plugins/sed.py
+++ ircbot/plugins/sed.py
@@ -1,13 +1,12 @@
 from ircbot import bot
 
-last_message = " "
+bot.last_message = " "
 
 
 @bot.hook()
 def sed_handler(bot, channel, sender, message):
-    global last_message
     if "s/" in message:
         sed = message.split("/")
         print(sed)
-        bot.message(channel, "<{}> {}".format(sender, last_message.replace(sed[1], sed[2])))
-    last_message = message
+        bot.message(channel, "<{}> {}".format(sender, bot.last_message.replace(sed[1], sed[2])))
+    bot.last_message = message
