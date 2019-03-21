d8a9048eb628599697894d62068425be0640dcd2
Cameron Sharp <me@cazagen.me>
2019-03-21 22:57:30 +0000

    Log wang level, and change wang level on wang activation

--- ircbot/plugins/numberwang.py
+++ ircbot/plugins/numberwang.py
@@ -1,15 +1,19 @@
 import random
+import logging
 
 from ircbot import bot
 
+logger = logging.getLogger(__name__)
+
 wang_counter = 0
-wang_level = random.randint(4, 10)
+wang_level = random.randint(6, 15)
 wang_ratio = 0.4
 
 
 @bot.hook()
 def message_hook(bot, channel, sender, message):
-    global wang_counter
+    global wang_counter, wang_level
+    logger.debug(wang_level)
 
     number_count = 0
     message_length = len(message)
@@ -23,4 +27,5 @@ def message_hook(bot, channel, sender, message):
 
     if wang_counter == wang_level:
         wang_counter = 0
-        bot.message(channel, "NUMBERWANG")
+        wang_level = random.randint(6, 15)
+        bot.message(channel, "That's Numberwang!")
