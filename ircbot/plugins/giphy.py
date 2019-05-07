cae19191560077f013706a8c9bf312b481c6a364
Cameron Sharp <me@cazagen.me>
2019-05-07 17:06:12 +0100

    Fix giphy api

--- ircbot/plugins/giphy.py
+++ ircbot/plugins/giphy.py
@@ -1,12 +1,19 @@
-from giphypop import screensaver
+import random
+import requests
 
 from ircbot import bot
+
+giphy_url = "https://api.giphy.com/v1/gifs/search"
+giphy_api = bot.config['Giphy']['api_key']
 
 
 @bot.command('giphy')
 def giphy(bot, channel, sender, args):
     """Usage: {bot.trigger}giphy [tag] - Finds a random gif, or returns a random gif tagged with [tag]"""
-    tag = None
-    if args:
-        tag = " ".join(args)
-    bot.message(channel, screensaver(tag=tag).fixed_height.url)
+
+    params = (('api_key', giphy_api), ('q', " ".join(args)), ('limit', '25'))
+    resp = requests.get(giphy_url, params=params).json()
+
+    gif_url = resp['data'][random.randint(1, 25)]['images']['original']['webp']
+
+    bot.message(channel, gif_url)
