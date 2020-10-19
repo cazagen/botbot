3f63e6cdb6c92b00df5486eb3bc944ec6e6baaf7
Cameron Sharp <me@cazagen.me>
2020-10-19 23:02:24 +0100

    Update random.cats url

--- ircbot/plugins/cats.py
+++ ircbot/plugins/cats.py
@@ -6,6 +6,6 @@ from ircbot import bot
 @bot.command('cat')
 def cat_command(bot, channel, sender, args):
     """Usage: {bot.trigger}cat - Posts a cat gif!"""
-    uri = "http://random.cat/meow"
+    uri = "https://aws.random.cat/meow"
     data = requests.get(uri).json()
     bot.message(channel, "{}".format(data['file']))
