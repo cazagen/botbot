2d2d66111c9d112a35849714449a7b108f545026
Cameron Sharp <me@cazagen.me>
2021-01-01 15:38:23 +0000

    Add ability to ignore certain websites in links plugin

--- ircbot/plugins/links.py
+++ ircbot/plugins/links.py
@@ -6,9 +6,9 @@ from ircbot import bot
 
 regex = re.compile(
         r'^(?:http|ftp)s?://' # http:// or https://
-        r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|' #domain...
-        r'localhost|' #localhost...
-        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})' # ...or ip
+        r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|' # domain
+        r'localhost|' # localhost
+        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})' # or ip
         r'(?::\d+)?' # optional port
         r'(?:/?|[/?]\S+)$', re.IGNORECASE)
 
@@ -44,6 +44,9 @@ def link_title_parse_hook(bot, channel, sender, message):
         return
 
     for word in message.split(" "):
+        for ignore in bot.config['Links']['ignore'].split():
+            if ignore in word.lower():
+                return
         try:
             if re.match(regex, word):
                 r = requests.get(word)
